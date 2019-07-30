dbfad13b43fe2088a858ef8004fe1bddf95b1631
wzlggg <wzl1888@126.com>
2019-07-30 17:27:11 +0800

    后台样式

--- pages/sechosevaluate/js/list.js
+++ pages/sechosevaluate/js/list.js
@@ -53,7 +53,7 @@ layui.use("table", function() {
 
         table.reload("testReload", {
           where: {
-            'evaluateDocVague': keyword.val()
+            evaluateDocVague: keyword.val()
           }
         });
       }
@@ -65,26 +65,26 @@ layui.use("table", function() {
   });
 
   //新增
-//   $("#infoAdd").on("click", function() {
-//     var data = "add";
-//     layer.open({
-//       type: 2,
-//       title: "iframe父子操作",
-//       maxmin: true,
-//       shadeClose: true, //点击遮罩关闭层
-//       area: ["469px", "300px"],
-//       content: "Edit.html",
-//       success: function(layero, index) {
-//         var body = layer.getChildFrame("body", index);
-//         var iframeWin = window[layero.find("iframe")[0]["name"]];
-//         iframeWin.inputDataHandle(data);
-//       },
-//       end: function() {
-//         //刷新页面
-//         window.location.reload();
-//       }
-//     });
-//   });
+  //   $("#infoAdd").on("click", function() {
+  //     var data = "add";
+  //     layer.open({
+  //       type: 2,
+  //       title: "iframe父子操作",
+  //       maxmin: true,
+  //       shadeClose: true, //点击遮罩关闭层
+  //       area: ["469px", "300px"],
+  //       content: "Edit.html",
+  //       success: function(layero, index) {
+  //         var body = layer.getChildFrame("body", index);
+  //         var iframeWin = window[layero.find("iframe")[0]["name"]];
+  //         iframeWin.inputDataHandle(data);
+  //       },
+  //       end: function() {
+  //         //刷新页面
+  //         window.location.reload();
+  //       }
+  //     });
+  //   });
 
   //删除
   $("#evaluateDel").on("click", function() {
@@ -130,7 +130,12 @@ layui.use("table", function() {
   });
 
   //编辑
-  var m_url = location.protocol + '\\\\' + location.hostname + ':' + (location.port == '' ? 80 : location.port);
+  var m_url =
+    location.protocol +
+    "\\\\" +
+    location.hostname +
+    ":" +
+    (location.port == "" ? 80 : location.port);
   table.on("tool(toolbar)", function(obj) {
     var value = obj.data;
     var patName;
@@ -139,60 +144,60 @@ layui.use("table", function() {
     var patAddress;
     var patMobile;
     $.ajax({
-        url:m_url+'/sys/patient/getPatientByGuid/'+ value.patientRowGuid,
-        contentType: 'application/json;charset=utf-8',
-		method: 'get',
-		data: JSON.stringify(value.patientRowGuid),
-        dataType: 'JSON',
-        success: function(res) {
-            if (res.code == '0') {
-                //console.log(res.data)
-                patName = res.data.patientName;
-                if(res.data.patientSex==0){
-                    patSex ='男' ;
-                }else{
-                    patSex ='女';
-                }
-                patIdCard = res.data.patientIdcard;
-                patAddress = res.data.patientAddress;
-                patMobile = res.data.patientMobile;
-            }
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-
+      url: m_url + "/sys/patient/getPatientByGuid/" + value.patientRowGuid,
+      contentType: "application/json;charset=utf-8",
+      method: "get",
+      data: JSON.stringify(value.patientRowGuid),
+      dataType: "JSON",
+      success: function(res) {
+        if (res.code == "0") {
+          //console.log(res.data);
+          patName = res.data.patientName;
+          if (res.data.patientSex == 0) {
+            patSex = "男";
+          } else {
+            patSex = "女";
+          }
+          patIdCard = res.data.patientIdcard;
+          patAddress = res.data.patientAddress;
+          patMobile = res.data.patientMobile;
         }
-    })
+      },
+      error: function(jqXHR, textStatus, errorThrown) {}
+    });
     if (obj.event === "edit") {
       //更新
       var data = "edit";
-      layer.open({
-        type: 2,
-        title: "详细信息",
-        maxmin: true,
-        shadeClose: true, //点击遮罩关闭层
-        area: ["560px", "450px"],
-        content: "Edit.html",
-        success: function(layero, index) {
-          var body = layer.getChildFrame("body", index);
-          var iframeWin = window[layero.find("iframe")[0]["name"]];
-          iframeWin.inputDataHandle(data);
-          body.find("#rowId").val(value.rowId);
-          body.find("#evaluateNurse").val(value.evaluateNurse);
-          body.find("#evaluateDoc").val(value.evaluateDoc);
-          body.find("#evaluateFacilities").val(value.evaluateFacilities);
-          body.find("#evaluateOpinion").val(value.evaluateOpinion);
-          //body.find("#patientRowGuid").val(value.patientRowGuid);
-          body.find("#patName").val(patName);
-          body.find("#patSex").val(patSex);
-          body.find("#patIdCard").val(patIdCard);
-          body.find("#patAddress").val(patAddress);
-          body.find("#patMobile").val(patMobile);
-        },
-        end: function() {
-          //刷新页面
-          window.location.reload();
-        }
-      });
+      setTimeout(function() {
+        layer.open({
+          type: 2,
+          title: "详细信息",
+          maxmin: true,
+          shadeClose: true, //点击遮罩关闭层
+          area: ["580px", "480px"],
+          content: "Edit.html",
+          success: function(layero, index) {
+            var body = layer.getChildFrame("body", index);
+            var iframeWin = window[layero.find("iframe")[0]["name"]];
+            iframeWin.inputDataHandle(data);
+            body.find("#rowId").val(value.rowId);
+            body.find("#evaluateNurse").val(value.evaluateNurse);
+            body.find("#evaluateDoc").val(value.evaluateDoc);
+            body.find("#evaluateFacilities").val(value.evaluateFacilities);
+            body.find("#evaluateOpinion").val(value.evaluateOpinion);
+            //body.find("#patientRowGuid").val(value.patientRowGuid);
+            body.find("#patName").val(patName);
+            body.find("#patSex").val(patSex);
+            body.find("#patIdCard").val(patIdCard);
+            body.find("#patAddress").val(patAddress);
+            body.find("#patMobile").val(patMobile);
+          },
+          end: function() {
+            //刷新页面
+            window.location.reload();
+          }
+        });
+      }, 500);
     }
   });
 });
