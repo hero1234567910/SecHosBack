816cce28be4ee565495375e7e753fa1f121c8acf
wzlggg <wzl1888@126.com>
2019-09-04 10:34:29 +0800

    修改

--- pages/home/js/console.js
+++ pages/home/js/console.js
@@ -1,33 +1,33 @@
 //创建vue实例化对象
-var vm = new Vue({
-    el: '#vueConfig',
-    data: {
-        orderCount:{}
-    },
-    created() {
-        //this.getRthing();
-    },
-    methods: {
-      //代办事项
-      getRthing(){
-      	let self = this;
-      	$.ajax({
-			url:prUrl+'/sys/hosorder/statistical',
-			contentType: 'application/json;charset=utf-8',
-			method: 'get',
-			dataType: 'JSON',
-			success: function(res) {
-				if (res.code == '0') {
-					self.orderCount = res.data;
-				}
-				if (res.code == '500') {
-					layer.msg(res.msg)
-				}
-			}
-		});
-      }
-    }
-})
+// var vm = new Vue({
+//     el: '#vueConfig',
+//     data: {
+//         orderCount:{}
+//     },
+//     created() {
+//         //this.getRthing();
+//     },
+//     methods: {
+//       //代办事项
+//       getRthing(){
+//       	let self = this;
+//       	$.ajax({
+// 			url:prUrl+'/sys/hosorder/statistical',
+// 			contentType: 'application/json;charset=utf-8',
+// 			method: 'get',
+// 			dataType: 'JSON',
+// 			success: function(res) {
+// 				if (res.code == '0') {
+// 					self.orderCount = res.data;
+// 				}
+// 				if (res.code == '500') {
+// 					layer.msg(res.msg)
+// 				}
+// 			}
+// 		});
+//       }
+//     }
+// })
 
 //改变接单状态
 function changeOrder(ele){
